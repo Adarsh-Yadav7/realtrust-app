744736ed21a227e147eb8abaaf5c15b6e3aa98a0
Adarsh Yadav <adarshyadav1705@gmail.com>
2025-12-09 21:00:59 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // backend base
+   baseURL: import.meta.env.VITE_API_BASE || "http://localhost:5000/api", // backend base
 });
 
 export default api;
